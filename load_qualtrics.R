ab5fc06f6816a95c7c49240608e6f552ce90ae8b
Lepennetier <GildasLepennetier@users.noreply.github.com>
2023-05-23 09:24:29 +0200

    add exemple

--- load_qualtrics.R
+++ load_qualtrics.R
@@ -1,5 +1,10 @@
 load_qx_excel <- function(infile, guess_max = 1E5){
 	# This function return a list with $data and $desc
+	# usage example:
+	#   XX <- load_qx_excel("file_from-QX.xlsx")
+	#   survey_allg <- XX$data ; survey_allg_desc <- XX$desc ; rm(XX)
+
+
 	DataNames <- readxl::read_excel(infile, n_max = 1, col_types = "text")
 	QX_names <- tibble::tibble(export_tag = names(DataNames), description = as.character(t(DataNames[1,])))
 	clinic_survey <- readxl::read_excel(infile, skip = 2, col_names = QX_names$export_tag, guess_max = guess_max)
